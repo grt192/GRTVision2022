1e03533340389edcdd81dde20d35a38bf27802e3
pinkbluesky <pinkbluesky@users.noreply.github.com>
2022-01-22 10:39:14 -0800

    Low latency HTTP and DS stream

--- simple_test.py
+++ simple_test.py
@@ -2,7 +2,6 @@ import threading
 import cv2
 import logging
 import time
-from cscore import CameraServer, MjpegServer, CvSource, VideoMode
 from pipeline import Pipeline
 from consumers.example_consumer import ExampleConsumer
 
@@ -41,6 +40,8 @@ def connect():
 
 roborio = connect()
 
+from cscore import CameraServer, MjpegServer, CvSource, VideoMode
+
 cam_server = CameraServer.getInstance()
 cam_server.enableLogging()
 
@@ -50,11 +51,13 @@ print('Attempting add a MjpegServer with name ')
 # access at address mjpg:http://10.1.92.94:1181/?action=stream
 server = cam_server.addServer(name='name')
 print('Completed attempt to add server with name at port ' + str(server.getPort()))
-stream = CvSource('name', VideoMode.PixelFormat.kMJPEG, 640, 360, 10)
+stream = CvSource('name', VideoMode.PixelFormat.kMJPEG, 16, 12, 10)
 server.setSource(stream)
 print('CvSource has been set for server at port ' + str(server.getPort()))
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
 
 while True:
     ret, frame = cap.read()
@@ -62,7 +65,9 @@ while True:
     if ret:
         stream.putFrame(frame)
     else:
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(1)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
         print('trying to capture again')
 
-    time.sleep(0.1)
+    time.sleep(0.001)
