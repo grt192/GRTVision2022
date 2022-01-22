2a9a7b5c5343a81bfb90c4762585c0ffd9ac49e0
pinkbluesky <pinkbluesky@users.noreply.github.com>
2022-01-21 19:07:49 -0800

    sorta working simple test

--- simple_test.py
+++ simple_test.py
@@ -1,6 +1,8 @@
 import threading
+import cv2
 import logging
 import time
+from cscore import CameraServer, MjpegServer, CvSource, VideoMode
 from pipeline import Pipeline
 from consumers.example_consumer import ExampleConsumer
 
@@ -22,7 +24,7 @@ def connect():
         with cond:
             notified[0] = True
             cond.notify()
-  
+
     # Use RoboRIO static IP address
     # Don't use 'roborio-192-frc.local'. https://robotpy.readthedocs.io/en/stable/guide/nt.html#networktables-guide
     NetworkTables.initialize(server='10.1.92.2')
@@ -39,20 +41,18 @@ def connect():
 
 roborio = connect()
 
-from cscore import CameraServer, MjpegServer, CvSource, VideoMode
-
 cam_server = CameraServer.getInstance()
 cam_server.enableLogging()
 
-print('Attempting add a MjpegServer with name ' + consumer.get_name())
+print('Attempting add a MjpegServer with name ')
 
 # add mjpegserver to the cameraserver
 # access at address mjpg:http://10.1.92.94:1181/?action=stream
-server = cam_server.addServer(name=consumer.get_name())
-print('Completed attempt to add server with name ' + consumer.get_name() + ' at port ' + str(server.getPort()))
-stream = CvSource(consumer.get_name(), VideoMode.PixelFormat.kMJPEG, consumer.stream_res()[0], consumer.stream_res()[1], consumer.fps())
+server = cam_server.addServer(name='name')
+print('Completed attempt to add server with name at port ' + str(server.getPort()))
+stream = CvSource('name', VideoMode.PixelFormat.kMJPEG, 640, 360, 10)
 server.setSource(stream)
-print('CvSource has been set for server ' + consumer.get_name() + ' at port ' + str(server.getPort()))
+print('CvSource has been set for server at port ' + str(server.getPort()))
 
 cap = cv2.VideoCapture(0)
 
@@ -61,7 +61,8 @@ while True:
 
     if ret:
         stream.putFrame(frame)
+    else:
+        cap = cv2.VideoCapture(0)
+        print('trying to capture again')
 
     time.sleep(0.1)
-
-
