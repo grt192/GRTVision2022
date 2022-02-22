c448d6939e2962fe42d616a5e095924fa5e30810
Ippyb <itzmeipsita@gmail.com>
2022-02-21 20:16:18 -0800

    played around with HSV values

--- local_intake_two.py
+++ local_intake_two.py
@@ -1,9 +1,3 @@
-import math
-import imutils
-import config
-import numpy as np
-import cv2
-import glob
 
 import cv2
 import numpy as np
@@ -27,33 +21,27 @@ while True:
     img = frame.copy()
     blur = cv2.blur(img, (4, 4))
     bilateral_filtered_image = cv2.bilateralFilter(blur, 5, 175, 175)
-    gray = cv2.cvtColor(bilateral_filtered_image, cv2.COLOR_BGR2GRAY)
-    #edge_detected_image = cv2.Canny(bilateral_filtered_image, 75, 200)
+    hsv = cv2.cvtColor(bilateral_filtered_image, cv2.COLOR_BGR2HSV)
 
-    # find all  contours in mask
-    try:
-        #TODO: test and get more accurate parameter values
-        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20,
-                                  param1=50, param2=30, minRadius=230, maxRadius=250)
-        circles = np.uint16(np.around(circles))
 
-        for i in circles[0, :]:
-            num_balls += 1
-            # draw the outer circle
-            cv2.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            # draw the center of the circle
-            cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 3)
 
-    except:
-        print("no circles detected")
+    #low_red = cv2.inRange(hsv, (0, 87, 0), (170, 87, 0))
+    high_red = cv2.inRange(hsv, (0, 130, 255), (180, 255, 255))
+    blue = cv2.inRange(hsv, (99, 71, 78), (123, 255, 255))
 
+    mask = cv2.bitwise_or(blue, high_red)
     cv2.circle(frame, cam_center, 2, (0, 255, 0), -1)
-    print("# of balls detected: " + str(num_balls))
 
     #cv2.imshow('Bilateral', bilateral_filtered_image)
     #cv2.imshow('detected circles', frame)
     #cv2.imshow('Edges', edge_detected_image)
-    cv2.imshow('Original', frame)
+    # cv2.imshow('Original', frame)
+    # cv2.imshow('Low_red', low_red)
+    # cv2.imshow('High_red', high_red)
+    # cv2.imshow('Blue', blue)
+    cv2.imshow('Bilateral', bilateral_filtered_image)
+    cv2.imshow("Mask", mask)
+
 
     key = cv2.waitKey(1)
     if key == 27:
@@ -61,3 +49,10 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+
+
+
+
